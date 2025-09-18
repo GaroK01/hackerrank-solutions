1e74b187d73902c56ec52be25d603c3efc5348cf
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-09-18 21:37:07 +0300

    Create Python: Division.py
    Python: Division solution.

---
+++ python/easy/Python: Division.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Perform arithmetic operations
+    
+    int_div = a // b
+    float_div = a / b
+    
+    # Print the results
+    
+    print(int_div)
+    print(float_div) 
