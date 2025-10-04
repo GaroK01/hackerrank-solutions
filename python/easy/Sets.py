df4198c9bacd84c9f6ba90c0a156fdc59568e9ef
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-10-04 20:55:40 +0300

    Update Sets.py
    Bug fixed, first remove the duplicates, then do your computations.

--- python/easy/Sets.py
+++ python/easy/Sets.py
@@ -1,12 +1,14 @@
 def average(array):
-    # Calculate the total sum of the array
-    total = sum(array)
+    # We need the distinct heights so we remove the duplicates by casting the array to a set
+    array_to_set = set(array)
+    # Caclculate the total sum of the set
+    total = sum(array_to_set)
     # Compute the average
-    average = total / len(array)
-    return average
+    average = total/ len(array_to_set)
+    return average 
 
 if __name__ == '__main__':
     n = int(input())
-    arr = list(map(int, input().split()))  
+    arr = list(map(int, input().split()))
     result = average(arr)
     print(result)
