881c4b2cd8dff9355a66b189664112d193d9bf1e
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-10-10 00:29:16 +0300

    Create Mutations.py
    Mutations solution.

---
+++ python/easy/Mutations.py
@@ -0,0 +1,10 @@
+def mutate_string(string, position, character):
+    # Add the character at a specific index of the string
+    string = string[:position] + character + string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
