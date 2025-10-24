b908883391cfd4061f5f92130a59cdefc969a48b
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-10-25 00:29:11 +0300

    Create Integers Come In All Sizes.py
    Come In All Sizes solution.

---
+++ python/easy/Integers Come In All Sizes.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+
+  # Take the inputs
+  
+  a = int(input())
+  b = int(input())
+  c = int(input())
+  d = int(input())
+
+  # Perform and print the operation
+  print(a**b + c**d)
