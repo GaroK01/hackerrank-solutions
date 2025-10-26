979236a7bb6dda3f5eb623dac1c1bb36c863f09d
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-10-26 22:35:44 +0200

    Update Set.intersection().py

--- python/easy/Set.intersection().py
+++ python/easy/Set.intersection().py
@@ -5,4 +5,4 @@ n_fr = int(input())
 set_fr = set(input().split())
 
 # Print the intersection (common elements) between the two sets
-print(len(s_fr.intersection(s_en)))
+print(len(set_fr.intersection(set_en)))
