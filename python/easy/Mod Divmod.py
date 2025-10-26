ef3a5d370d75f2a630d14d92c9038f71e367890e
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-10-26 22:53:12 +0200

    Create Mod Divmod.py
    Mod Divmod solution.

---
+++ python/easy/Mod Divmod.py
@@ -0,0 +1,8 @@
+dividend = int(input())
+divisor = int(input())
+
+print(int(dividend/divisor))
+print(dividend%divisor)
+
+# Prints a tuple that contains the quotient of the division and the remainder
+print(divmod(dividend,divisor))
